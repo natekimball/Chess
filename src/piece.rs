1b864dc5c0d2bb5ed3d859b89a403f103451100a
natekimball <natekimball1@gmail.com>
2022-12-20 14:30:05 -0500

    fixed en passant logic and refactored is_valid

--- src/piece.rs
+++ src/piece.rs
@@ -17,6 +17,9 @@ impl Piece {
         if from == to {
             return false;
         }
+        let last_move = game.can_en_passant();
+        game.set_can_enpassant(false);
+
         let (x, y) = (to.0 as i8 - from.0 as i8, to.1 as i8 - from.1 as i8);
         match self {
             Piece::Queen(_) => ((x == 0 || y == 0) && game.check_horiz(from, to)) || ((x.abs() == y.abs()) && game.check_diag(from, (x,y))),
@@ -25,27 +28,76 @@ impl Piece {
             Piece::Rook(_) => (x == 0 || y == 0) && game.check_horiz(from, to),
             Piece::Knight(_) => (x.abs() == 2 && y.abs() == 1) || (x.abs() == 1 && y.abs() == 2),
             Piece::Pawn(player) => {
-                match player {
-                    Player::Player1 => {
-                        let forwards = if from.1==1 { ((x,y) == (0, 1) && game.check_horiz(from, (from.0,from.1+2))) || ((x,y) == (0,2) && game.check_horiz(from, (from.0,from.1+3))) } else { (x,y) == (0, 1) };
-                        let diag = (x.abs() == 1) && (y == 1);
-                        if diag {
-                            if game.check_spot_for_opponent(to) {
-                                return true;
-                            } else if to.1==5 && game.check_spot_for_opponent((to.0,4)) {
-                                game.take(to);
-                                return true;
-                            }
-                            return false;
-                        }
-                        forwards
-                    },
-                    Player::Player2 => {
-                        let forwards = if from.1==6 { ((x,y) == (0, -1) && game.check_horiz(from, (from.0,from.1-2))) || ((x,y) == (0,-2) && game.check_horiz(from,(from.0,from.1-3))) } else { (x,y) == (0, -1) };
-                        forwards || ((x.abs() == 1) && (y == 1) && (game.check_spot_for_opponent(to) || (to.1==2 && game.check_spot_for_opponent((to.0,3)))))
+                let sign = match player {
+                    Player::One => 1,
+                    Player::Two => -1
+                };
+                let end = match player {
+                    Player::One => 6,
+                    Player::Two => 1
+                };
+                let single = (x,y) == (0, sign*1) && game.check_horiz(from, (from.0, (from.1 as i8 + sign*2) as u8));
+                let double = from.1==(end as i8-sign*5) as u8 && (x,y) == (0, sign*2) && game.check_horiz(from, (from.0, (from.1 as i8 + sign*3) as u8));
+                if double {
+                    game.set_can_enpassant(true);
+                    return true;
+                }
+                let diag = (x.abs() == 1) && (y == sign*1);
+                dbg!(last_move);
+                dbg!(to.1, (end as i8 - 2*sign) as u8);
+                dbg!((to.0,(end as i8 + sign*2) as u8));
+                if diag {
+                    if game.check_spot_for_opponent(to) {
+                        return true;
+                    } else if last_move && to.1==(end as i8 - sign) as u8 && game.check_spot_for_opponent((to.0,(end as i8 - sign*2) as u8)) {
+                        game.take((to.0,(end as i8 - 2*sign) as u8), None);
+                        return true;
+                    } else {
+                        return false;
                     }
                 }
+                single
             }
+                    // Player::One => {
+                    //     let single = (x,y) == (0, 1) && game.check_horiz(from, (from.0, from.1+2));
+                    //     let double = from.1==6 && (x,y) == (0, -2) && game.check_horiz(from, (from.0, from.1+3));
+                    //     if double {
+                    //         game.set_can_enpassant(true);
+                    //         return true;
+                    //     }
+                    //     let diag = (x.abs() == 1) && (y == 1);
+                    //     if diag {
+                    //         if game.check_spot_for_opponent(to) {
+                    //             return true;
+                    //         } else if last_move && to.1==5 && game.check_spot_for_opponent((to.0,4)) {
+                    //             game.take((to.0,4), None);
+                    //             return true;
+                    //         } else {
+                    //             return false;
+                    //         }
+                    //     }
+                    //     single
+                    // },
+                    // Player::Two => {
+                    //     let single = (x,y) == (0, -1) && game.check_horiz(from, (from.0, (from.1 as i8 -2) as u8));
+                    //     let double = from.1==6 && (x,y) == (0, -2) && game.check_horiz(from, (from.0, (from.1 as i8 -3) as u8));
+                    //     if double {
+                    //         game.set_can_enpassant(true);
+                    //         return true;
+                    //     }
+                    //     let diag = (x.abs() == 1) && (y == -1);
+                    //     if diag {
+                    //         if game.check_spot_for_opponent(to) {
+                    //             return true;
+                    //         } else if last_move && to.1==2 && game.check_spot_for_opponent((to.0,3)) {
+                    //             game.take((to.0,3), None);
+                    //             return true;
+                    //         } else {
+                    //             return false;
+                    //         }
+                    //     }
+                    //     single
+                    // }
         }
     }
 
@@ -56,7 +108,7 @@ impl Piece {
             Piece::Rook(player) => *player,
             Piece::Bishop(player) => *player,
             Piece::Knight(player) => *player,
-            Piece::Pawn(player) => *player,
+            Piece::Pawn(player) => *player
         }
     }
 }
@@ -64,13 +116,12 @@ impl Piece {
 impl Display for Piece {
     fn fmt(&self, f: &mut Formatter) -> Result<(), Error> {
         match self {
-            // Piece::King(player) => write!(f, " K{} ", player.num()),
-            Piece::King(player) => write!(f, " {} ", if player.num() == 1 { format!("K").red().bold() } else { format!("K").blue().bold() }),
-            Piece::Queen(player) => write!(f, " {} ", if player.num() == 1 { format!("Q").red().bold() } else { format!("Q").blue().bold() }),
-            Piece::Rook(player) => write!(f, " {} ", if player.num() == 1 { format!("R").red().bold() } else { format!("R").blue().bold() }),
-            Piece::Bishop(player) => write!(f, " {} ", if player.num() == 1 { format!("B").red().bold() } else { format!("B").blue().bold() }),
-            Piece::Knight(player) => write!(f, " {} ", if player.num() == 1 { format!("N").red().bold() } else { format!("N").blue().bold() }),
-            Piece::Pawn(player) => write!(f, " {} ", if player.num() == 1 { format!("P").red().bold() } else { format!("P").blue().bold() }),
+            Piece::King(player) => write!(f, " {} ", if *player == Player::One { format!("K").red().bold() } else { format!("K").blue().bold() }),
+            Piece::Queen(player) => write!(f, " {} ", if *player == Player::One { format!("Q").red().bold() } else { format!("Q").blue().bold() }),
+            Piece::Rook(player) => write!(f, " {} ", if *player == Player::One { format!("R").red().bold() } else { format!("R").blue().bold() }),
+            Piece::Bishop(player) => write!(f, " {} ", if *player == Player::One { format!("B").red().bold() } else { format!("B").blue().bold() }),
+            Piece::Knight(player) => write!(f, " {} ", if *player == Player::One { format!("N").red().bold() } else { format!("N").blue().bold() }),
+            Piece::Pawn(player) => write!(f, " {} ", if *player == Player::One { format!("P").red().bold() } else { format!("P").blue().bold() }),
         }
     }
 }
