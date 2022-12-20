604a7872fc7c254e2b12cb43a26dbc887ac4cd39
natekimball <natekimball1@gmail.com>
2022-12-20 18:27:06 -0500

    stopped user from moving into check

--- src/game.rs
+++ src/game.rs
@@ -1,7 +1,5 @@
 use std::{io, fmt::{Display, Formatter, Error}, cmp::{min, max}};
-
 use colored::Colorize;
-
 use crate::piece::Piece;
 
 pub struct Game {
@@ -10,6 +8,7 @@ pub struct Game {
     game_over: bool,
     last_move_double: bool
 }
+
 impl Game {
     pub fn new() -> Game {
         let mut board = Vec::new();
@@ -60,33 +59,34 @@ impl Game {
 
         let mut valid_move = false;
         while !valid_move {
-            let mut input = String::new();
-            io::stdin().read_line(&mut input).unwrap();
-            let mut input = input.split_whitespace();
-            let from = input.next().unwrap().to_ascii_lowercase();
-            let to = input.next().unwrap().to_ascii_lowercase();
-            let from = (from.chars().nth(0).unwrap() as u8 - 'a' as u8, from.chars().nth(1).unwrap() as u8 - '1' as u8);
-            let to = (to.chars().nth(0).unwrap() as u8 - 'a' as u8, to.chars().nth(1).unwrap() as u8 - '1' as u8);
-
+            let (from, to) = get_move();
             if self.is_current_player(from) {
                 // if self.board[from.1 as usize][from.0 as usize].as_ref().unwrap().is_valid(from, to, self) {
-                if let Some(piece) = self.get(from) { 
-                    if !piece.is_valid(from, to, self) {
-                        println!("Invalid move! go again.");
-                        continue;
-                    }
-                    if let Some(conquered) = self.get(to) {
-                        if conquered.player() == self.current_player {
-                            println!("You can't take your own piece!");
-                            continue;
-                        } else {
-                            self.take(to, Some(piece));
-                        }
-                    }
-                    self.board[to.1 as usize][to.0 as usize] = self.board[from.1 as usize][from.0 as usize];
-                    self.board[from.1 as usize][from.0 as usize] = None;
-                    valid_move = true;
+                let piece = self.get(from);
+                let conquered = self.get(to);
+                if !piece.unwrap().is_valid(from, to, self) {
+                    println!("Invalid move! go again.");
+                    continue;
                 }
+                if let Some(conquered) = conquered {
+                    if conquered.player() == self.current_player {
+                        println!("You can't take your own piece! go again.");
+                        continue;
+                    } else {
+                        self.take(to, piece);
+                        self.set(from, None);
+                    }
+                } else {
+                    self.set(to, piece);
+                    self.set(from, None);
+                }
+                if self.player_in_check() {
+                    println!("You can't put yourself in check!");
+                    self.set(from, self.get(to));
+                    self.set(to, conquered);
+                    continue;
+                }
+                valid_move = true;
             } else {
                 println!("You must move one of your own pieces!");
             }
@@ -148,7 +148,7 @@ impl Game {
     pub(crate) fn check_diag(&self, from: (u8, u8), delta: (i8, i8)) -> bool {
         let signs = (delta.0.signum(), delta.1.signum());
         for i in 1..delta.0.abs() as u8 {
-            if self.board[(from.1 as i8 + signs.1 * i as i8) as usize][(from.0 as i8 + signs.0 * i as i8) as usize].is_some() {
+            if self.get(((from.0 as i8 + signs.0 * i as i8) as u8, (from.1 as i8 + signs.1 * i as i8) as u8)).is_some() {
                 return false;
             }
         }
@@ -156,16 +156,16 @@ impl Game {
     }
 
     pub(crate) fn check_spot_for_opponent(&self, to: (u8, u8)) -> bool {
-        if let Some(piece) = self.board[to.1 as usize][to.0 as usize] {
+        if let Some(piece) = self.get(to) {
             piece.player() != self.current_player
         } else {
             false
         }
     }
 
-    pub(crate) fn take(&mut self, to: (u8, u8), new: Option<Piece>) {
-        let piece = self.board[to.1 as usize][to.0 as usize].unwrap();
-        self.board[to.1 as usize][to.0 as usize] = new;
+    pub(crate) fn take(&mut self,  to: (u8, u8), new_piece: Option<Piece>) {
+        let piece = self.get(to).unwrap();
+        self.set(to, new_piece);
         println!("You took {}'s {}!", match self.current_player {
             Player::One => Player::Two,
             Player::Two => Player::Two,
@@ -194,6 +194,10 @@ impl Game {
         self.board[y as usize][x as usize]
     }
 
+    pub(crate) fn set(&mut self, (x, y): (u8, u8), piece: Option<Piece>) {
+        self.board[y as usize][x as usize] = piece;
+    }
+
     pub(crate) fn in_check(&mut self, king: (u8, u8)) -> bool {
         for i in 0..8 {
             for j in 0..8 {
@@ -212,7 +216,7 @@ impl Game {
     fn get_king(&self) -> (u8, u8) {
         for i in 0..8 {
             for j in 0..8 {
-                if let Some(piece) = self.board[i][j] {
+                if let Some(piece) = self.get((j,i)) {
                     if piece.player() == self.current_player && piece.is_king() {
                         return (j as u8, i as u8);
                     }
@@ -221,6 +225,21 @@ impl Game {
         }
         panic!("No king found!");
     }
+
+    fn player_in_check(&mut self) -> bool {
+        self.in_check(self.get_king())
+    }
+}
+
+fn get_move() -> ((u8, u8), (u8, u8)) {
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+    let mut input = input.split_whitespace();
+    let from = input.next().unwrap().to_ascii_lowercase();
+    let to = input.next().unwrap().to_ascii_lowercase();
+    let from = (from.chars().nth(0).unwrap() as u8 - 'a' as u8, from.chars().nth(1).unwrap() as u8 - '1' as u8);
+    let to = (to.chars().nth(0).unwrap() as u8 - 'a' as u8, to.chars().nth(1).unwrap() as u8 - '1' as u8);
+    (from, to)
 }
 
 // TODO: make set method, use get method
