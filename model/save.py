c8c2b4f677f2fe3daae4d494c23fe96cc1d5ab3e
Nate Kimball <tma5gv@virginia.edu>
2023-05-18 00:20:00 -0400

    save with signatures

--- model/save.py
+++ model/save.py
@@ -2,14 +2,9 @@ import tensorflow as tf
 from keras.models import load_model
 import util
 
-output_dir = 'saved_model_t'
-# model = load_model('model_v4')
+output_dir = 'model_v4_w_sigs'
+model = load_model('model_v4')
 
-# util.save_signatures(model, output_dir)
+util.save_signatures(model, output_dir)
 model = tf.saved_model.load(output_dir)
 print(model.signatures)
-
-checkpoint = tf.train.Checkpoint(model)
-checkpoint.read("training_checkpoints/ckpt").assert_consumed()
-
-# now what
